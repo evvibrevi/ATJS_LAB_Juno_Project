258682843be9d2c928dde10a20c6e02bebd6674a
keshah716 <keshah716@gmail.com>
2025-04-07 11:43:15 +0400

    Configured reporter for ReportPortal

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  
+
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -22,7 +24,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }], // html
+    ['@reportportal/agent-js-playwright', {
+       token: 'JunoTest_g7XAEQnpR4yesrtEXCSbfgS-sAK9cwscjiKvwcP7Q_yFxtATjhc8LcYWkTZqpRP2',
+      endpoint: 'https://reportportal.epam.com/api/v1',
+      project: 'ruslan_klimakov_personal', 
+      launch: 'Playwright UI tests',
+      description: 'Juno test',
+      attributes: [{ key: 'team', value: 'QA' }],
+      debug: false,
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -39,15 +52,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+ 
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
